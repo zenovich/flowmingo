1545813590a470b17b35b5c89991c683ed0eb705
Dmitry Zenovich <dzenovich@gmail.com>
2024-07-06 08:10:47 +0300

    get rid of github.com/stretchr/testify

--- capture_test.go
+++ capture_test.go
@@ -7,8 +7,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/zenovich/flowmingo"
 )
 
@@ -29,11 +27,11 @@ func TestCaptureStdoutAndStderr_CapturesAllWritesInChronologicalOrder_WithOutput
 			}()
 
 			outR, outW, err := os.Pipe()
-			assert.NoError(t, err)
+			assertNoError(t, err)
 			os.Stdout = outW
 
 			errR, errW, err := os.Pipe()
-			assert.NoError(t, err)
+			assertNoError(t, err)
 			os.Stderr = errW
 
 			restore := flowmingo.CaptureStdoutAndStderr()
@@ -49,37 +47,37 @@ func TestCaptureStdoutAndStderr_CapturesAllWritesInChronologicalOrder_WithOutput
 
 			chunks := restore(doOutput)
 
-			assert.Equal(t, 5, len(chunks))
-			assert.Equal(t, "ab", string(chunks[0].Chunk))
-			assert.Equal(t, "cde", string(chunks[1].Chunk))
-			assert.Equal(t, "fgh", string(chunks[2].Chunk))
-			assert.Equal(t, "i", string(chunks[3].Chunk))
-			assert.Equal(t, "jkl", string(chunks[4].Chunk))
-			assert.Equal(t, os.Stdout, chunks[0].OutFile)
-			assert.Equal(t, os.Stderr, chunks[1].OutFile)
-			assert.Equal(t, os.Stdout, chunks[2].OutFile)
-			assert.Equal(t, os.Stderr, chunks[3].OutFile)
-			assert.Equal(t, os.Stdout, chunks[4].OutFile)
+			assertEqualInts(t, 5, len(chunks))
+			assertEqualStrings(t, "ab", string(chunks[0].Chunk))
+			assertEqualStrings(t, "cde", string(chunks[1].Chunk))
+			assertEqualStrings(t, "fgh", string(chunks[2].Chunk))
+			assertEqualStrings(t, "i", string(chunks[3].Chunk))
+			assertEqualStrings(t, "jkl", string(chunks[4].Chunk))
+			assertEqualFiles(t, os.Stdout, chunks[0].OutFile)
+			assertEqualFiles(t, os.Stderr, chunks[1].OutFile)
+			assertEqualFiles(t, os.Stdout, chunks[2].OutFile)
+			assertEqualFiles(t, os.Stderr, chunks[3].OutFile)
+			assertEqualFiles(t, os.Stdout, chunks[4].OutFile)
 
 			_ = outW.Close()
 			var outBuf bytes.Buffer
 			_, err = io.Copy(&outBuf, outR)
-			assert.NoError(t, err)
+			assertNoError(t, err)
 			expectedStdout := "abfghjkl"
 			if !doOutput {
 				expectedStdout = ""
 			}
-			assert.Equal(t, expectedStdout, outBuf.String())
+			assertEqualStrings(t, expectedStdout, outBuf.String())
 
 			_ = errW.Close()
 			var errBuf bytes.Buffer
 			_, err = io.Copy(&errBuf, errR)
-			assert.NoError(t, err)
+			assertNoError(t, err)
 			expectedStderr := "cdei"
 			if !doOutput {
 				expectedStderr = ""
 			}
-			assert.Equal(t, expectedStderr, errBuf.String())
+			assertEqualStrings(t, expectedStderr, errBuf.String())
 		})
 	}
 }
@@ -87,15 +85,54 @@ func TestCaptureStdoutAndStderr_CapturesAllWritesInChronologicalOrder_WithOutput
 func TestRestoreFunc_DoesntAllowToBeCalledTwice(t *testing.T) {
 	restoreFunc := flowmingo.Capture(os.Stdout)
 	restoreFunc(true)
-	assert.Panics(t, func() { restoreFunc(true) })
+	assertPanics(t, func() { restoreFunc(true) })
 }
 
 func TestRestoreFunc_ChecksIfOutputIsReplaced(t *testing.T) {
 	restoreFunc1 := flowmingo.CaptureStdoutAndStderr()
 	restoreFunc2 := flowmingo.Capture(os.Stderr)
 
-	assert.Panics(t, func() { restoreFunc1(true) })
+	assertPanics(t, func() { restoreFunc1(true) })
 
 	restoreFunc2(false)
 	restoreFunc1(true)
+}
+
+func assertEqualStrings(t *testing.T, expected, actual string) {
+	if expected != actual {
+		t.Errorf("Not equal: \n"+
+			"expected: %s\n"+
+			"actual  : %s", expected, actual)
+	}
+}
+
+func assertEqualInts(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Not equal: \n"+
+			"expected: %d\n"+
+			"actual  : %d", expected, actual)
+	}
+}
+
+func assertEqualFiles(t *testing.T, expected, actual *os.File) {
+	if expected != actual {
+		t.Errorf("Not equal: \n"+
+			"expected: %v\n"+
+			"actual  : %v", expected, actual)
+	}
+}
+
+func assertNoError(t *testing.T, err error) {
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+}
+
+func assertPanics(t *testing.T, f func()) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+	f()
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/zenovich/flowmingo
 
 go 1.0
-
-require github.com/stretchr/testify v1.7.0
